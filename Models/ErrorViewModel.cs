497407e5cc9eb805f1f3b070df76db48e48387af
geraltofrivia69 <charneuk@gmail.com>
2022-08-28 18:25:59 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuperSmashApp3CosmosDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
